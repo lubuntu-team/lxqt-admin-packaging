64aadfcbdbaf44c8013e0da4ffb71880473889fc
Andrew Lee (李健秋) <ajqlee@debian.org>
2015-08-15 01:41:18 +0800

    Merging upstream version 0.9.0+20150807.

--- lxqt-admin-user/userdialog.cpp
+++ lxqt-admin-user/userdialog.cpp
@@ -21,11 +21,14 @@
 #include "userdialog.h"
 #include <QMessageBox>
 
+#define DEFAULT_UID_MIN 1000
+#define DEFAULT_UID_MAX 32768
+
 UserDialog::UserDialog(OobsUser* user, QWidget* parent):
     QDialog(),
+    mUser(user ? OOBS_USER(g_object_ref(user)) : NULL),
     mFullNameChanged(false),
-    mHomeDirChanged(false),
-    mUser(user ? OOBS_USER(g_object_ref(user)) : NULL)
+    mHomeDirChanged(false)
 {
     ui.setupUi(this);
 
@@ -66,7 +69,7 @@ UserDialog::UserDialog(OobsUser* user, QWidget* parent):
         ui.fullName->setText(oobs_user_get_full_name(user));
         ui.loginShell->setEditText(oobs_user_get_shell(user));
         ui.homeDir->setText(QString::fromLocal8Bit(oobs_user_get_home_directory(user)));
-        
+
         OobsGroup* group = oobs_user_get_main_group(user);
         ui.mainGroup->setEditText(oobs_group_get_name(group));
     }
@@ -76,11 +79,11 @@ UserDialog::UserDialog(OobsUser* user, QWidget* parent):
         ui.loginName->setReadOnly(false);
         ui.loginName->setFocus();
         ui.changePasswd->setChecked(true);
-        ui.uid->setValue(oobs_users_config_find_free_uid(userConfig, 1000, 32768));
+        ui.uid->setValue(oobs_users_config_find_free_uid(userConfig, DEFAULT_UID_MIN, DEFAULT_UID_MAX));
         ui.loginShell->setEditText(oobs_users_config_get_default_shell(userConfig));
         ui.mainGroup->setCurrentIndex(-1);
     }
-    
+
 }
 
 UserDialog::~UserDialog()
@@ -97,7 +100,7 @@ void UserDialog::onLoginNameChanged(const QString& text)
         ui.fullName->setText(text);
         ui.fullName->blockSignals(false);
     }
-    
+
     if(!mHomeDirChanged)
     {
         ui.homeDir->blockSignals(true);
